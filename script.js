d457bce3b630e9192eaff47193e305b5920088c2
fedepaloz <palozzifederico@gmail.com>
2022-06-21 08:37:03 +0100

    prompt

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21
  */
 
-console.log
+
+const firstName = prompt('quale è il tuo nome?');
+
+const secondName = prompt('quale è il tuo cognome?');
+
+const favColor = prompt('il tuo colore preferito');
+
+const codice = firstName + secondName + favColor
 
 
+
+resultText.innerText = codice;
